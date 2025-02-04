0a75e890470603c07a5c56fea7b594ad5bcf8f66
THNDRNoob <ajwhitex2008@outlook.com>
2025-02-04 10:23:37 -0700

    the new functions! woa

--- PictureTester.java
+++ PictureTester.java
@@ -11,8 +11,9 @@ public class PictureTester {
      * Method to test the PictureExplorer
      */
     public static void testExplorer() {
-        Picture beach = new Picture("beach.jpg");
-        beach.explore();   
+        Picture p = new Picture("myPicture.jpg");
+        Picture smallP = p.scale(0.25,0.25);
+        smallP.write("smallMyPicture.jpg");
     }
 
     /**
